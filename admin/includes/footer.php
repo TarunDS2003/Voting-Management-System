e5fd75e8456091bbec4ee97fdd9f28d44d5f18ff
Tarun DS <tarunds2003@gmail.com>
2024-10-15 14:37:21 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2024 <a href="https://github.com/TarunDS2003">Tarun DS</a></strong>
+</footer>
